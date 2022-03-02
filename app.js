bc7ca0d566708067a0d6ceb84e2aee3a91c847c3
Cody-Wise <cody@sharptackmedia.com>
2022-03-01 16:25:39 -0800

    Upddated data and app files

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 // import functions
 
+import { people } from './data.js';
+
+// eslint-disable-next-line no-console
+console.log(people, 'people');
+
 // grab DOM elements
+
+const selectE1 = document.querySelector('#name');
+const containE1 = document.querySelector('div');
+const nameE1 = document.querySelector('h5');
+const favplaceE1 = document.querySelector('span');
+const hobbiesE1 = document.querySelector('ul');
+
+console.log(selectE1);
+console.log(containE1);
+console.log(nameE1);
+console.log(favplaceE1);
+console.log(hobbiesE1);
 
 // set event listeners 
     // get user input
---
+++ data.js
@@ -0,0 +1,31 @@
+export const people = [
+
+    {
+        id: 1,
+        name: 'Cody',
+        pronouns: 'they/them',
+        favorite_place: 'Miami, FL',
+        hobbies: ['hiking', 'cooking', 'coding'],
+    },
+    {
+        id: 2,
+        name: 'Ian',
+        pronouns: 'she/her',
+        favorite_place: 'couch',
+        hobbies: ['knitting', 'sewing', 'coding'],
+    },
+    {
+        id: 3,
+        name: 'Khayman',
+        pronouns: 'he/him',
+        favorite_place: 'Stinson Beach',
+        hobbies: ['basketball', 'gaming', 'coding'],
+    },
+    {
+        id: 4,
+        name: 'Haley',
+        pronouns: 'ze/zim',
+        favorite_place: 'Tahoma',
+        hobbies: ['painting', 'cooking', 'skiing'],
+    },
+];
